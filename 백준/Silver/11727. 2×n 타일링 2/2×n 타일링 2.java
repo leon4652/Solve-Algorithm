2ca00fe75706bc60fbda995666cfa7234c6c083c
leon4652 <93763809+leon4652@users.noreply.github.com>
2023-03-18 21:05:39 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 216 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static long d[] = new long[1000 + 1];
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		d[1] = 1;
+		d[2] = 3;
+		
+		for (int i = 3; i < (1000 + 1); i++) {
+			d[i] = (2 * d[i - 2] + d[i - 1]) % 10007;
+		}
+		
+		System.out.println(d[N]);
+	}
+
+}
